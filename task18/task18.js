6a7af508842df57492fd2e6a2c2916318edbc607
ukeyim <ukeyim@gmail.com>
2016-09-26 23:55:57 +0800

    queue with closure

--- task18/task18.js
+++ task18/task18.js
@@ -8,4 +8,42 @@ function addEventHandler(element,event,listener){
 	else{
 		element["on" + event] = listener;
 	}
+};
+array = new Array();
+function queueOperate(){
+	num = document.getElementById("num").value;
+	leftin:function(num){
+		array.shift(num);
+		render();
+	}
+	rightin:function(num){
+		array.push(num);
+		render();
+	}
+	leftout: function(){
+		if(array.length<=0){
+			alert('please input a number');
+		}else{
+			array.unshift(0);
+		}
+		render();
+	}
+	rightout:function(){
+		if(arrya.length<=0){
+			alert('please input a number');
+		}else{
+			array.pop();
+		}
+		render();
+	}
+}
+
+function render(){
+	var container = document.getElementById("container");
+	var str = '';
+	array.forEach(function(x,index,a){
+		str += "<div style='height:20px;width:60px;background:#fc625d;'>" + x +"</div>";
+
+
+	});
 }
